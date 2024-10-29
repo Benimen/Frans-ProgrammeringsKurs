aa14161a6e78031e55afdb1ef6655731c35a7979
Benimen <bmolina7475@gmail.com>
2024-10-29 14:19:28 +0100

    Added a function for assignment 6

--- Program1.py
+++ Program1.py
@@ -9,4 +9,18 @@ if b > a:
     print("This is ab comparison")
 
 
-print("After ab")
+def hantera_text(text):
+    # Skriv ut det första tecknet
+    print("Första tecknet:", text[0])
+
+    #Skriv ut tecken från position 7 till 12
+    print("Tecken från position 7 till 12", text[7:12])
+
+    #Skriv ut varannat tecken
+    print("Skriv ut varannat teckn", text[::2])
+
+    #Skriv ut strängen baklänges
+    print("Skriv ut strängen baklänges", text[::-1])
+
+
+hantera_text("Hello World!")
