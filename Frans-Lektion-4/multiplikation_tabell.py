3afb1b15fe4c78867f23e55fe28ab1cee019ec83
Benimen <bmolina7475@gmail.com>
2024-11-03 00:35:52 +0100

    Init commit

--- Frans-Lektion-4/multiplikation_tabell.py
+++ Frans-Lektion-4/multiplikation_tabell.py
@@ -1,9 +1,24 @@
-number = int(input("Multiply: "))
-up_to = int(input("up to: "))
 
-def multiplication_table(number, up_to):
-    for i in range(1, up_to + 1):
-        print(f"{number} x {i} = {number * i}")
+def multiplication_table():
+    number = input("Value to multiply?: ")
+    up_to = input("What max value?: ")
+    
+    
+    if number.isdigit() and up_to.isdigit():
+     
+        return int(number), int(up_to)
+    
+    else: return None, None
 
-multiplication_table(number, up_to)
 
+
+number, up_to = multiplication_table()
+
+if number is not None and up_to is not None:
+    if int(number) <= int(up_to):
+        for i in range(1, int(up_to / number) + 1):
+            print(f"{number} x {i} = {number * i}")
+    else:
+        print("Base value needs to be bigger than max value")
+else:
+    print("Multiplication table creation failed due to invalid input.")
