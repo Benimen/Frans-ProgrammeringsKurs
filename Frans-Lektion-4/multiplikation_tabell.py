7de7736afd1e48f690aa37a69901d9fa8e90fa7e
Benimen <bmolina7475@gmail.com>
2024-11-02 22:37:37 +0100

    changed placement of input

--- Frans-Lektion-4/multiplikation_tabell.py
+++ Frans-Lektion-4/multiplikation_tabell.py
@@ -1,8 +1,9 @@
-
+number = int(input("Multiply: "))
+up_to = int(input("up to: "))
 
 def multiplication_table(number, up_to):
-    """ Generate a multiplication table for a given number up to a specified range."""
     for i in range(1, up_to + 1):
         print(f"{number} x {i} = {number * i}")
 
-multiplication_table(number = int(input("Multiply: ")), up_to = int(input("How many times to multiply: ")))
+multiplication_table(number, up_to)
+
