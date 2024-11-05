cf6b8fae301c6e5abc822996a09c208636f42819
Benimen <bmolina7475@gmail.com>
2024-11-05 16:34:44 +0100

    init commit

--- Frans-lektion-5/calc_function.py
+++ Frans-lektion-5/calc_function.py
@@ -1,1 +1,52 @@
-print("Temp")
+
+
+def calculate(*args, operation ="add"):
+    if not args:
+        return 0
+    
+
+    if not all(isinstance(arg, (int, float)) for arg in args):
+        return "Error, must be number: "
+
+
+    if operation == "+":
+        return sum(args)
+    
+
+    elif operation == "-":
+        result = args[0]
+        for num in args[1:]:
+            result -= num
+        return result
+    
+
+    elif operation == "*":
+        result = 1
+        for num in args:
+            result *= num
+        return result
+    
+
+    elif operation == "/":
+        result = args[0]
+        for num in args[1:]:
+            if num == 0:
+                return "Error, can not divide with 0"
+            result /= num
+        return result
+    
+
+    else:
+        return "Error, Invalid operation"
+    
+numbers_input = input("Enter numbers and separate with space: ")
+operation_input = input("Enter operation (+, -, *, /): ")
+
+
+try:
+    numbers = [float(num) for num in numbers_input.split()]
+except ValueError:
+    print("Error: Enter valid number.")
+else:
+    result = calculate(*numbers, operation=operation_input)
+    print("Result:", result)
